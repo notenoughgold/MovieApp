deb210abbfb85c3e1ba7a5632d19626fc890d018
notenoughgold <nurshat@hotmail.com>
2019-07-21 16:06:49 +0300

    minor fixes

--- app/src/main/java/altayiskender/movieapp/repository/TmdbApi.kt
+++ app/src/main/java/altayiskender/movieapp/repository/TmdbApi.kt
@@ -3,27 +3,26 @@ package altayiskender.movieapp.repository
 import altayiskender.movieapp.models.Movie
 import altayiskender.movieapp.models.Movies
 import altayiskender.movieapp.models.PeopleResponse
-import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface TmdbApi {
     @GET("movie/popular")
-    fun getPopularMovies(): Deferred<Movies>
+    suspend fun getPopularMovies(): Movies
 
     @GET("movie/upcoming")
-    fun getUpcomingMovies(): Deferred<Movies>
+    suspend fun getUpcomingMovies(): Movies
 
     @GET("movie/now_playing")
-    fun getNowPlayingMovies(): Deferred<Movies>
+    suspend fun getNowPlayingMovies(): Movies
 
     @GET("movie/{movie_id}?append_to_response=credits,videos")
-    fun getMovieDetails(@Path(value = "movie_id")movieId: Long): Deferred<Movie>
+    suspend fun getMovieDetails(@Path(value = "movie_id") movieId: Long): Movie
 
     @GET("search/movie")
-    fun searchMovie(@Query("query") query: String): Deferred<Movies>
+    suspend fun searchMovie(@Query("query") query: String): Movies
 
-    @GET("person/{person_id}?append_to_response=combined_credits")
-    fun getPeopleDetails(@Path(value = "person_id")personId:Long):Deferred<PeopleResponse>
+    @GET("person/{person_id}?append_to_response=movie_credits")
+    suspend fun getPeopleDetails(@Path(value = "person_id") personId: Long): PeopleResponse
 }
